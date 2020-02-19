da93ed4ead0149327fef46b10fc16f7b658380f8
ArtemPetrenkoEduardovich <Artem7888@gmail.com>
2020-02-19 17:28:42 +0200

    1

--- src/main/resources/bd.sql
+++ src/main/resources/bd.sql
@@ -14,7 +14,7 @@ create table IF NOT EXISTS gruppa(
 );
 
 insert gruppa(name)
-values ("Group 1"), ("Group 2"), ("Group 3"), ("Group 4"), ("Group 5")
+values ("Group 1"), ("Group 2"), ("Group 3"), ("Group 4"), ("Group 5");
 
 insert students(name, birth_date, address_town, address_street, address_house)
 values
@@ -22,5 +22,5 @@ values
 ("Student 2", "2002-02-02", "Town 2", "Street 2", "House 2"),
 ("Student 3", "2003-03-03", "Town 3", "Street 3", "House 3"),
 ("Student 4", "2004-04-04", "Town 4", "Street 4", "House 4"),
-("Student 5", "2005-05-05", "Town 5", "Street 5", "House 5")
+("Student 5", "2005-05-05", "Town 5", "Street 5", "House 5");
 
